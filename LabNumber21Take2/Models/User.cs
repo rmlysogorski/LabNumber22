bfe676d0450f83aa648619a9e11d8229eb569b19
Rich Lysogorski <rmlysogorski@gmail.com>
2019-05-28 08:45:58 -0400

    First commit

---
+++ LabNumber21Take2/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabNumber21Take2.Models
+{
+    public class User
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phoneNumber;
+        private string password;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public User()
+        {
+
+        }
+
+        public User(string _firstName, string _lastName, string _email, string _phoneNumber, string _password)
+        {
+            FirstName = _firstName;
+            LastName = _lastName;
+            Email = _email;
+            PhoneNumber = _phoneNumber;
+            Password = _password;
+        }
+    }
+}
